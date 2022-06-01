450f708269c7abd778e75747daff138416e28e0c
AnastasiyaSam <anastasiya.samoilenka@hyperisland.se>
2022-06-01 18:03:03 +0200

    first lvl

---
+++ script.js
@@ -0,0 +1,24 @@
+let right_code = "5926";
+
+/////////////// buttons with code
+function button_click(btn) {
+  btn.innerHTML = (parseInt(btn.innerHTML) + 1) % 10;
+}
+
+function check_code() {
+  let code = "";
+  for (let i = 0; i < 4; i++) {
+    code += document.getElementById("button_" + i.toString()).innerHTML;
+  }
+  if (code == right_code) {
+    alert("Right code! You found a key inside!");
+    localStorage.setItem("have_key", "true");
+  } else {
+    alert("Wrong code!");
+  }
+  window.location.href = "Index.html";
+}
+
+function go_back() {
+  window.location.href = "Index.html";
+}
